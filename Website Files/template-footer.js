f73b9346f52c8f826cf8413fc814cbcd2f51894a
jenseni-git <jenseniain@gmail.com>
2023-02-21 23:01:24 +1000

    Moving stuff around and adding /Documentation

---
+++ Website Files/template-footer.js
@@ -0,0 +1,18 @@
+document.write('		</div>\
+		<div id="footer">\
+			<div id="footerContactDetails">\
+				<p>OrigamiHub inc.</p>\
+				<p>0444 444 444</p>\
+				<p>origami.hub@protonmail.ch</p>\
+			</div>\
+			<div id="footerMap">\
+				<div id="footerMapText">\
+					<p>Come visit our warehouse at</p>\
+					<p>123 Fake Street</br>Oak Lawn, Illinois</br>60453</br>USA</p>\
+				</div>\
+				<div id="googleMap">\
+					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d552.5262358586796!2d-87.74883963123949!3d41.72535392550655!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e3bd3bb6db18d%3A0x6582323d7a833d16!2s123%20Fake%20Street%2C%20Oak%20Lawn%2C%20IL%2060453%2C%20USA!5e0!3m2!1sen!2sau!4v1653530938458!5m2!1sen!2sau" width="100" height="100" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>\
+				</div>\
+			</div>\
+		</div>\
+		<script type="text/javascript" src="script.js"></script>');
